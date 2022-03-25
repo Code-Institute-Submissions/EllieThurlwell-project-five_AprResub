608e9dc9e59925b0bec975b330eba67b1d19d8e0
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2022-03-25 19:51:57 +0000

    Add current category and search results displays

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ def all_products(request):
     """ view to display all products """
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
