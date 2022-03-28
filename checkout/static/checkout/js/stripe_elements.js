824e89b93e45f8a76847a90d24657a3e04aa0b6a
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2022-03-28 10:54:26 +0000

    Add stripe public and secret keys

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,6 @@
 // stripe core logic
 
+// create stripe card element
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
@@ -22,3 +23,19 @@ var style = {
   };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// handle card element errors in realtime
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
