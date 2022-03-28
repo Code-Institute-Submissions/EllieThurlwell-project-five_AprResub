abb25575288785ff09e3b54e9812640046550aaa
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2022-03-28 13:11:17 +0000

    Add order form submit functionality

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 
 def checkout(request):
     """ view to render the checkout page """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket right now")
@@ -18,13 +21,22 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KiF2fGktD06S5XUJvh4hD88j6A08ui4n6Ty902qOSOFhSkW0kx49guph7CV7hya5Skvq2tKRNzoxWnPb2znoyj000dD01QyzE',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
