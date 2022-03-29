1da7e9aa38d825060299213e111621c427a83e61
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2022-03-29 09:18:01 +0000

    Add cache control for AWS

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """ tell django to use S3 when running collectstatic """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """ tell django to use S3 when running collectstatic """
     location = settings.MEDIAFILES_LOCATION
